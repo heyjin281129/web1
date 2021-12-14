a50db72bc44a3f49799ed5160894264d4211a1b2
혜진 <87840587+heyjin281129@users.noreply.github.com>
2021-12-14 23:03:40 +0900

    [바닐라js] 끝

--- 바닐라JS로_그림판_만들기/app.js
+++ 바닐라JS로_그림판_만들기/app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -76,6 +77,14 @@ function handleCM(event) {
   event.preventDefault();
 }
 
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[EXPORT]";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -95,4 +104,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
